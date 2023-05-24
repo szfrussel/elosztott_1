f129a8d64d8099ff819ed4937947acd931580f5b
Szabo Ferenc <szfrussel2@gmail.com>
2023-05-24 21:11:58 +0200

    első local comit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello - bello  - world!");
         System.out.println("one local commit");
+        System.out.println("two local commit");
     }
 }
